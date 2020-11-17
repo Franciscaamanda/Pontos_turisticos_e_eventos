1f39f196995b76333f381044b1985e943c8cab0d
Gabriel Oliveira <gabriel.assunsas@gmail.com>
2020-11-17 01:10:07 -0300

    alter main class name

--- src/main/kotlin/api/base/repository/database/mongodb/ICollection.kt
+++ src/main/kotlin/api/base/repository/database/mongodb/ICollection.kt
@@ -3,26 +3,27 @@ package api.base.repository.database.mongodb
 import api.base.models.evento.Evento
 import api.base.models.usuarios.UsuarioComum
 import api.base.models.usuarios.UsuarioAnunciante
+import api.base.repository.database.IGetCollection
 import com.mongodb.client.MongoCollection
 import org.litote.kmongo.getCollection
 
 
-interface IColUsuarioComum: MongoDBConnection {
-    fun getCollection(collectionName: String): MongoCollection<UsuarioComum> {
+interface IColUsuarioComum: IGetCollection, MongoDBConnection {
+    override fun getCollection(collectionName: String): MongoCollection<UsuarioComum> {
         val conn = connect()
         return conn.getCollection<UsuarioComum>(collectionName)
     }
 }
 
-interface IColUsuarioAnunciante: MongoDBConnection {
-    fun getCollection(collectionName: String): MongoCollection<UsuarioAnunciante> {
+interface IColUsuarioAnunciante: IGetCollection, MongoDBConnection {
+    override fun getCollection(collectionName: String): MongoCollection<UsuarioAnunciante> {
         val conn = connect()
         return conn.getCollection<UsuarioAnunciante>(collectionName)
     }
 }
 
-interface IColEvento: MongoDBConnection {
-    fun getCollection(collectionName: String): MongoCollection<Evento> {
+interface IColEvento: IGetCollection, MongoDBConnection {
+    override fun getCollection(collectionName: String): MongoCollection<Evento> {
         val conn = connect()
         return conn.getCollection<Evento>(collectionName)
     }
